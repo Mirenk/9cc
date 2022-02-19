beb4e7704598d370411ca97c92bc25f6055a4167
Mirenk <minikmwinsea@gmail.com>
2022-02-19 21:43:55 +0900

    分割の変更
    * まちがえてました

--- parse.c
+++ parse.c
@@ -1,152 +1,129 @@
-#include <ctype.h>
-#include <stdarg.h> // 可変個引数の処理に必要
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "9cc.h"
 
-// エラーを報告するための関数
-// printfと同じ引数を取る
-void error(char *fmt, ...) { // 可変個引数を利用
-    va_list ap; // 可変個の引数をリストとして格納
-    va_start(ap, fmt); // apを可変個引数の1番目に向ける
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
-    exit(1);
-}
+Node *code[100]; // 構文木の先頭を保存しておく配列
 
-// エラー箇所を報告する
-void error_at(char *loc, char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
+Node *mul();
+Node *primary();
+Node *unary();
+Node *add();
+Node *equality();
+Node *relational();
 
-    int pos = loc - user_input; // アドレスなので、loc(エラー箇所) >= user_input(入力文字列の先頭)
-    fprintf(stderr, "%s\n", user_input);
-    fprintf(stderr, "%*s", pos, " ");// pos個の空白を出力
-    fprintf(stderr, "^ ");
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
-    exit(1);
-}
-
-// 次のトークンが期待している記号のときには、トークンを1つ読み進めて
-// 真を返す。それ以外の場合には偽を返す
-bool consume(char *op) {
-    if(token->kind != TK_RESERVED ||
-       strlen(op) != token->len ||
-       memcmp(token->str, op, token->len)) {
-        return false;
-    }
-    token = token->next;
-    return true;
-}
-
-// トークンがTK_IDENTの場合はトークンのポインタを返す
-Token *consume_ident() {
-    Token *ident_token = token;
-    if(token->kind != TK_IDENT) {
-        return NULL;
-    }
-    token = token->next;
-    return ident_token;
-}
-
-// 次のトークンが期待している記号のときには、トークンを1つ読み進める。
-// それ以外の場合にはエラーを報告する。
-void expect(char *op) {
-    if(token->kind != TK_RESERVED ||
-       strlen(op) != token->len ||
-       memcmp(token->str, op, token->len)) {
-        error_at(token->str, "'%s'ではありません", op);
-    }
-    token = token->next;
-}
-
-// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
-// それ以外の場合にはエラーを報告する。
-int expect_number() {
-    if(token->kind != TK_NUM) {
-        error_at(token->str, "数ではありません");
-    }
-    int val = token->val;
-    token = token->next;
-    return val;
-}
-
-bool at_eof() {
-    return token->kind == TK_EOF;
-}
-
-// 新しいトークンを作成してcurに繋げる
-Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
-    Token *tok = calloc(1, sizeof(Token));
-    tok->kind = kind;
-    tok->str = str;
-    tok->len = len;
-    cur->next = tok;
-    return tok;
-}
-
-bool startswith(char *p, char *q) {
-    return memcmp(p, q, 2) == 0;
-}
-
-// 入力文字列pをトークナイズしてそれを返す
-Token *tokenize(char *p) {
-    Token head; // こいつは空で、head->nextから中身あり
-    head.next = NULL;
-    Token *cur = &head;
-
-    while(*p) {
-        // 空白文字をスキップ
-        if(isspace(*p)) {
-            p++;
-            continue;
-        }
-
-        if('a' <= *p && 'z' >= *p) { // 文字がa~zの間の場合
-            cur = new_token(TK_IDENT, cur, p++, 1);
-            cur->len = 1;
-            continue;
-        }
-
-        if(startswith(p, "==") || startswith(p ,"!=") || startswith(p, ">=") || startswith(p, "<=")) {
-            cur = new_token(TK_RESERVED, cur, p, 2);
-            p += 2;
-            continue;
-        }
-
-        if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')' || *p == '<' || *p == '>' || *p == '=' || *p == ';') {
-            cur = new_token(TK_RESERVED, cur, p++, 1); // 記号が来たら記号を入れたあとにpを進める
-            continue;
-        }
-
-        if(isdigit(*p)) {
-            cur = new_token(TK_NUM, cur, p, 0);
-            char *tmp = p;
-            cur->val = strtol(p, &p, 10);
-            cur->len = p - tmp;
-            continue;
-        }
-
-        error_at(p, "トークナイズできません");
+Node *primary() {
+    // 次のトークンが"("なら、"(" expr ")"のはず
+    if(consume("(")) {
+        Node *node = expr();
+        expect(")");
+        return node;
     }
 
-    new_token(TK_EOF, cur, p, 0);
-    return head.next; // 中身があるやつの先頭を返して上げる
+    // 次のトークンが変数の場合
+    Token *tok = consume_ident();
+    if(tok) {
+        Node *node = calloc(1, sizeof(Node));
+        node->kind = ND_LVAR;
+        node->offset = (tok->str[0] - 'a' + 1) * 8; // ASCIIでオフセットを決める，aならrbp-8,bならrbp-16…
+        return node;
+    }
+
+    // そうでなければ数値のはず
+    return new_node_num(expect_number());
 }
 
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
-    Node *node = calloc(1, sizeof(Node));
-    node->kind = kind;
-    node->lhs = lhs;
-    node->rhs = rhs;
+Node *unary() {
+    if(consume("+")) {
+        return primary(); // 単項+の場合、+だけ進めてprimaryを呼ぶ
+    }
+    if(consume("-")) {
+        return new_node(ND_SUB, new_node_num(0), primary()); // 単項-の場合、0-xとする
+    }
+    return primary(); // その他の場合、今までと同じ
+}
+
+Node *mul() {
+    Node *node = unary();
+
+    for(;;) {
+        if(consume("*")) {
+            node = new_node(ND_MUL, node, unary());
+        } else if(consume("/")) {
+            node = new_node(ND_DIV, node, unary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *add() {
+    Node *node = mul();
+
+    for(;;) {
+        if(consume("+")){
+            node = new_node(ND_ADD, node, mul());
+        } else if(consume("-")) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *relational() {
+    Node *node = add();
+
+    for(;;) {
+        if(consume("<")) {
+            node = new_node(ND_LT, node, add());
+        } else if(consume(">")) {
+            node = new_node(ND_LT, add(), node); // 項を逆にして対応
+        } else if(consume("<=")) {
+            node = new_node(ND_LE, node, add());
+        } else if(consume(">=")) {
+            node = new_node(ND_LE, add(), node); // これも逆にする
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *equality() {
+    Node *node = relational();
+
+    for(;;) {
+        if(consume("==")) {
+            node = new_node(ND_EQ, node, relational());
+        } else if(consume("!=")) {
+            node = new_node(ND_NE, node, relational());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *assign() {
+    Node *node = equality();
+    if(consume("=")) {
+        node = new_node(ND_ASSIGN, node, assign());
+    }
     return node;
 }
 
-Node *new_node_num(int val) {
-    Node *node =calloc(1, sizeof(Node));
-    node->kind = ND_NUM;
-    node->val = val;
+Node *expr() {
+    return assign();
+}
+
+Node *stmt() {
+    Node *node = expr();
+    expect(";");
     return node;
+}
+
+void program() {
+    int i = 0;
+    while(!at_eof()) {
+        code[i++] = stmt(); // トークンが終わるまで木を作成し，入れていく
+    }
+    code[i] = NULL; // 最後の木の後にnullを入れ，末尾が分かるようにする
 }
