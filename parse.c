e69c9004e9208d5a3872305fe7f9d6bd210a9ce2
mirenk <minikmwinsea@gmail.com>
2022-05-11 20:19:23 +0900

    ファイル分割ミスの修正

--- parse.c
+++ parse.c
@@ -3,6 +3,21 @@
 #include <string.h>
 #include "9cc.h"
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node =calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
 LVar *locals; // ローカル変数
 
 LVar *find_lvar(Token *tok) {
@@ -21,8 +36,6 @@ Node *add();
 Node *equality();
 Node *relational();
 Node *expr();
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
-Node *new_node_num(int val);
 
 Node *primary() {
     // 次のトークンが"("なら、"(" expr ")"のはず
--- tokenizer.c
+++ tokenizer.c
@@ -190,17 +190,3 @@ Token *tokenize(char *p) {
     return head.next; // 中身があるやつの先頭を返して上げる
 }
 
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
-    Node *node = calloc(1, sizeof(Node));
-    node->kind = kind;
-    node->lhs = lhs;
-    node->rhs = rhs;
-    return node;
-}
-
-Node *new_node_num(int val) {
-    Node *node =calloc(1, sizeof(Node));
-    node->kind = ND_NUM;
-    node->val = val;
-    return node;
-}
